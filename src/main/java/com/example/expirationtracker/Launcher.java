7281b5ad66b04ba6558e6e027c9684ff39ff8632
Aryab Adhikari <aaryabadhikari07@gmail.com>
2026-06-17 19:12:19 -0400

    Initial commit

---
+++ src/main/java/com/example/expirationtracker/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.expirationtracker;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.expirationtracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.expirationtracker to javafx.fxml;
+    exports com.example.expirationtracker;
+}
