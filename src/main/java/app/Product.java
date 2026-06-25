ae5ed38cceef638a313e2a792d3da2d0d15725cc
Aryab Adhikari <aryab.adhikari07@gmail.com>
2026-06-25 15:08:43 -0400

    Creating a product class and playing around with it. This product class is like a blueprint to all the products

---
+++ src/main/java/app/Product.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.time.LocalDate;
+
+public class Product {
+    int id;
+    String name;
+    String category;
+    int quantity;
+    LocalDate purchaseDate;
+    LocalDate expirationDate;
+    String notes;
+}
